5b744951d42056b4f012c4669be993ffa95334cc
Hyunsoo Kim <HyunsooKim@Hyunsoos-MacBook-Pro-3.local>
2020-09-26 10:50:13 -0700

    ch

--- import_and_model.py
+++ import_and_model.py
@@ -46,7 +46,7 @@ def model_input():
 
 
     try:
-        s3.Bucket(BUCKET_NAME).download_file(KEY, "sample_mp3.mp3")
+        s3.download_file(BUCKET_NAME, KEY, "sample_mp3.mp3")
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
@@ -76,6 +76,8 @@ def model_input():
     
     #Create a DF that will take the created Melspectogram directory
     data_df = pd.DataFrame([{'bird': "sample bird", 'song_sample': f"/app/upload_mel/{filename}"}])
+    
+    # Users/HyunsooKim/Desktop/Boot_Camp/Homework/BIRD_CALL/upload_mel/{filename}"}])
     
     #Compile the model
     callbacks = [ReduceLROnPlateau(monitor='val_loss', patience=2, verbose=1, factor=0.7),
